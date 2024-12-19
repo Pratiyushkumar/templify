4151e86c97353fe30e4c4bcf3bd72f5884e6ad88
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-20 00:14:57 +0530

    removed try now from the /home

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react';
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { Button } from './button';
 
 const Navbar: FC = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  console.log(currentPath);
+  
   return (
     <nav className="fixed top-0 w-full bg-gray-800/20 backdrop-blur-sm border-b border-gray-700/30 z-50">
       <div className="container flex h-14 items-center justify-between py-8 px-14">
@@ -14,11 +18,11 @@ const Navbar: FC = () => {
           </h1>
         </div>
 
-        <Button className="w-32 py-6" variant="default">
+        {currentPath !== '/home' && <Button className="w-32 py-6" variant="default">
         <Link to="/home">
                 <p className='text-lg py-4 tracking-wider'> Try Now</p>
               </Link>
-        </Button>
+        </Button>}
       </div>
     </nav>
   );
