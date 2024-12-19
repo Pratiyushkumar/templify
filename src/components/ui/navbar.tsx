e1a5d9cf5ec5cf408d02e09b13687e76871a64ef
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-20 00:21:19 +0530

    add responsiveness in nav

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -9,22 +9,27 @@ const Navbar: FC = () => {
   
   return (
     <nav className="fixed top-0 w-full bg-gray-800/20 backdrop-blur-sm border-b border-gray-700/30 z-50">
-      <div className="container flex h-14 items-center justify-between py-8 px-14">
-        <div className="flex items-center gap-2">
-          <h1 className="text-4xl font-bold text-blue-500 tracking-wider">
-            <Link to="/">
-              Templify
-            </Link>
-          </h1>
-        </div>
+  <div className="container flex h-14 items-center  justify-center md:justify-between py-4 px-6 md:py-8 md:px-10 lg:px-14">
+    <div className="flex  items-center gap-2">
+      <h1 className=" text-2xl md:text-3xl lg:text-4xl text-center  font-bold text-blue-500 tracking-wider">
+        <Link to="/">Templify</Link>
+      </h1>
+    </div>
 
-        {currentPath !== '/home' && <Button className="w-32 py-6" variant="default">
-        <Link to="/home">
-                <p className='text-lg py-4 tracking-wider'> Try Now</p>
-              </Link>
-        </Button>}
-      </div>
-    </nav>
+    
+    {/* Button displayed on larger screens */}
+    <div className="hidden md:block">
+      {currentPath !== "/home" && (
+        <Button className="w-24 md:w-32 py-2 md:py-6" variant="default">
+          <Link to="/home">
+            <p className="text-sm md:text-lg py-1 md:py-4 tracking-wider">Try Now</p>
+          </Link>
+        </Button>
+      )}
+    </div>
+  </div>
+</nav>
+
   );
 };
 
