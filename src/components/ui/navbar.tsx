a412426971b0c61c80e253fdd4bca9e1911aed10
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-19 21:38:16 +0530

    fix styling

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -5,17 +5,19 @@ import { Button } from './button';
 const Navbar: FC = () => {
   return (
     <nav className="fixed top-0 w-full bg-gray-800/20 backdrop-blur-sm border-b border-gray-700/30 z-50">
-      <div className="container flex h-14 items-center justify-between p-6">
+      <div className="container flex h-14 items-center justify-between py-8 px-14">
         <div className="flex items-center gap-2">
-          <h1 className="text-2xl font-bold text-white">
+          <h1 className="text-4xl font-bold text-blue-500 tracking-wider">
             <Link to="/">
               Templify
             </Link>
           </h1>
         </div>
 
-        <Button className="w-32" variant="default">
-          Try now!
+        <Button className="w-32 py-6" variant="default">
+        <Link to="/home">
+                <p className='text-lg py-4 tracking-wider'> Try Now</p>
+              </Link>
         </Button>
       </div>
     </nav>
