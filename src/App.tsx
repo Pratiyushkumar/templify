ce9836c9448fb9884fb3e7ca8173b77e36ec1b68
Pratiyush Kumar <pratiyush.inv@gmail.com>
2024-12-19 09:35:37 +0530

    Routing fixes has been done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
 import Home from './page/Home';
 import SignIn from './page/SignIn';
 import SignUp from './page/SignUp';
 import Hero from './page/Hero';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   );
 }
 
--- src/page/Hero.tsx
+++ src/page/Hero.tsx
@@ -1,7 +1,7 @@
 import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect"
 import { BackgroundBeams } from "../components/ui/background-beams";
 import Navbar from "../components/ui/navbar";
-
+import { Link } from "react-router";
 
 const words = [
   {
@@ -36,24 +36,26 @@ const words = [
 function Hero() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className="h-[100vh] w-full flex flex-col bg-neutral-950 items-center justify-center   ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        Upload your files, define a template, and let us do the rest—convert your data into customized text or PDFs in no time!
-      </p>
-      <TypewriterEffectSmooth words={words} />
-      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button className="w-32 h-10 rounded-xl bg-blue-500 border dark:border-white border-transparent font-semibold text-white text-sm z-50">
-          Try Now!
-        </button>
-        <button className="w-32 h-10 rounded-xl font-semibold bg-white text-black border border-black text-sm z-50">
-          Signup
-        </button>
-      </div>
-        <BackgroundBeams/>
+        <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+          Upload your files, define a template, and let us do the rest—convert your data into customized text or PDFs in no time!
+        </p>
+        <TypewriterEffectSmooth words={words} />
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+          <button className="w-32 h-10 rounded-xl bg-blue-500 border dark:border-white border-transparent font-semibold text-white text-sm z-50">
+            Try Now!
+          </button>
+          <button className="w-32 h-10 rounded-xl font-semibold bg-white text-black border border-black text-sm z-50">
+            <Link to="/signup">
+              Signup
+            </Link >
+          </button>
+        </div>
+        <BackgroundBeams />
       </div>
     </div>
-    
+
   );
 }
 
