5ca8bf77cb0b7440dc836b026d450ae3890fe801
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-20 19:20:19 +0530

    Implemeted BackgroundGrid and code refactor

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,20 +1,30 @@
-import { BsCloudUpload } from "react-icons/bs";
-import UploadedFileInfo from "./UploadedFileInfo";
+import { BsCloudUpload } from "react-icons/bs"
+import UploadedFileInfo from "./UploadedFileInfo"
 
-import { FaRegCircleCheck } from "react-icons/fa6";
-import useFileUploader from "../hooks/useFileUploader";
+import { FaRegCircleCheck } from "react-icons/fa6"
+import useFileUploader from "../hooks/useFileUploader"
+import BackgroundGrid from "./BackgroundGrid"
 interface FileUploaderProps {
   onFileSelected: (file: File) => void
 }
 
-
 const FileUploader: React.FC<FileUploaderProps> = ({ onFileSelected }) => {
-  const {handleDragLeave, handleDragOver, handleDrop, handleFileChange, error,file, inputRef, dragging} = useFileUploader({onFileSelected});
+  
+  const {
+    handleDragLeave,
+    handleDragOver,
+    handleDrop,
+    handleFileChange,
+    error,
+    file,
+    inputRef,
+    dragging,
+  } = useFileUploader({ onFileSelected })
   return (
-    <div className="flex flex-col items-center my-8">
-    <div
-      onClick={() => inputRef.current?.click()}
-      className={`
+    <div className="flex flex-col items-center my-8 ">
+      <div
+        onClick={() => inputRef.current?.click()}
+        className={`relative
         w-full max-w-md h-48 rounded-lg border-2 border-dashed
         ${
           dragging
@@ -24,58 +34,69 @@ const FileUploader: React.FC<FileUploaderProps> = ({ onFileSelected }) => {
         flex flex-col justify-center items-center text-center
         transition-all duration-300 cursor-pointer
       `}
-      onDragOver={handleDragOver}
-      onDragLeave={handleDragLeave}
-      onDrop={handleDrop}
-    >
-      <input
-        ref={inputRef}
-        id="fileInput"
-        type="file"
-        accept=".xlsx, .xls, .csv"
-        onChange={handleFileChange}
-        className="hidden"
-      />
-      {file ? (
-        <div className="px-5 space-y-2">
-          <div className="flex items-center  text-gray-300 gap-2">
-            <FaRegCircleCheck className=" w-3 h-3" />
-            <p className="text-sm tracking-wider normal-case">File uploaded successfully</p>
-        </div>
-          <UploadedFileInfo file={file} />
-        </div>
-      ) : (
-        <div className="flex flex-col items-center gap-1">
-          <div className="flex flex-col items-center space-x-2 text-gray-400 gap-1">
-            <BsCloudUpload className="text-gray-400 w-7 h-7 md:w-8 md:h-8" />
-            <p className="text-[14px] md:text-[16px] dark:text-gray-400">
-              <span className="underline underline-offset-2">Click to upload</span> or drag &
-              drop
-            </p>
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <input
+          ref={inputRef}
+          id="fileInput"
+          type="file"
+          accept=".xlsx, .xls, .csv"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+
+        <BackgroundGrid />
+        
+        {file ? (
+          <div className="px-5 space-y-2">
+            <div className="flex items-center  text-gray-300 gap-2">
+              <FaRegCircleCheck className=" w-3 h-3" />
+              <p className="text-sm tracking-wider normal-case">
+                File uploaded successfully
+              </p>
+            </div>
+            <UploadedFileInfo file={file} />
           </div>
-          <div className="text-gray-400 space-y-2 md:space-y-1">
-            <p className="text-sm md:text-[12px] md:text-sm">Maximum file size is 10 MB</p>
-            <p className="text-[12px] md:text-sm">Accepted file formats are:</p>
+        ) : (
+          <div className="flex flex-col items-center gap-1">
+            <div className="flex flex-col items-center space-x-2 text-gray-400 gap-1">
+              <BsCloudUpload className="text-gray-400 w-7 h-7 md:w-8 md:h-8" />
+              <p className="text-[14px] md:text-[16px] dark:text-gray-400">
+                <span className="underline underline-offset-2">
+                  Click to upload
+                </span>{" "}
+                or drag & drop
+              </p>
+            </div>
+            <div className="text-gray-400 space-y-2 md:space-y-1">
+              <p className="text-sm md:text-[12px] md:text-sm">
+                Maximum file size is 10 MB
+              </p>
+              <p className="text-[12px] md:text-sm">
+                Accepted file formats are:
+              </p>
+            </div>
+
+            <div className="flex space-x-3 mt-1">
+              <img
+                src="../../src/assets/icons/csv-file.png"
+                alt="CSV"
+                className="w-8 h-8"
+              />
+              <img
+                src="../../src/assets/icons/xls.png"
+                alt="XLSX"
+                className="w-8 h-8"
+              />
+            </div>
           </div>
-  
-          <div className="flex space-x-3 mt-1">
-            <img
-              src="../../src/assets/icons/csv-file.png"
-              alt="CSV"
-              className="w-8 h-8"
-            />
-            <img
-              src="../../src/assets/icons/xls.png"
-              alt="XLSX"
-              className="w-8 h-8"
-            />
-          </div>
-        </div>
-      )}
+        )}
+      </div>
+
+      {error && <p className="mt-2 text-red-500 text-sm">{error}</p>}
     </div>
-  
-    {error && <p className="mt-2 text-red-500 text-sm">{error}</p>}
-  </div>
   )
 }
 
