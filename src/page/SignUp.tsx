aa1231f7c89884444669a6dc0878b98477faaed8
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-20 17:00:44 +0530

    css and signin route fixed

--- src/page/SignUp.tsx
+++ src/page/SignUp.tsx
@@ -1,15 +1,16 @@
 
+import { useNavigate } from 'react-router';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import useSignup from '../hooks/useSignup';
 
 const SignUp = () => {
   const {formData,errorMessage, loading, handleChange, handleSubmit,} = useSignup();
-
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen bg-gray-950">
       <div className="flex items-center justify-center px-4 py-24">
-        <div className="w-full max-w-md space-y-6 bg-gray-900/50 p-8 rounded-lg border border-gray-800">
+        <div className="w-full max-w-md space-y-4 bg-gray-900/50 p-8 rounded-lg border border-gray-800">
           <div className="space-y-2 text-center">
             <h1 className="text-2xl font-bold tracking-tighter text-white">
               Create an account
@@ -27,7 +28,7 @@ const SignUp = () => {
                 name="name"
                 value={formData.name}
                 onChange={handleChange}
-                required
+            
               />
             </div>
             <div>
@@ -38,7 +39,7 @@ const SignUp = () => {
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
-                required
+            
               />
             </div>
             <div>
@@ -49,7 +50,7 @@ const SignUp = () => {
                 name="password"
                 value={formData.password}
                 onChange={handleChange}
-                required
+            
               />
             </div>
             <div>
@@ -60,7 +61,7 @@ const SignUp = () => {
                 name="confirmPassword"
                 value={formData.confirmPassword}
                 onChange={handleChange}
-                required
+            
               />
             </div>
             {errorMessage && (
@@ -70,15 +71,15 @@ const SignUp = () => {
             )}
             <Button
               type="submit"
-              className="w-full bg-blue-600 hover:bg-blue-700"
+              className="w-full bg-blue-600 hover:bg-blue-700 h-12"
               disabled={loading}
             >
-              {loading ? 'Creating Account...' : 'Sign Up'}
+              {loading ? <p className='md:text-lg tracking-wide '>Creating account</p> : <p className='md:text-lg tracking-wide '>Sign up</p>}
             </Button>
           </form>
           <div className="text-center text-sm text-gray-400">
             Already have an account?{' '}
-            <button onClick={() => console.log('Navigate to signin')} className="text-blue-400 hover:text-blue-300">
+            <button onClick={() => navigate("/signin")} className="text-blue-400 hover:text-blue-300">
               Sign in
             </button>
           </div>
