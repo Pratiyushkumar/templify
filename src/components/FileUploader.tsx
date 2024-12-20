7acb97abde985ba82132e626d563a09a9b5e5b4f
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-20 18:54:41 +0530

    implemented useFileUploader

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,69 +1,15 @@
-import { ChangeEvent, useRef, useState, DragEvent } from "react"
 import { BsCloudUpload } from "react-icons/bs";
 import UploadedFileInfo from "./UploadedFileInfo";
 
 import { FaRegCircleCheck } from "react-icons/fa6";
+import useFileUploader from "../hooks/useFileUploader";
 interface FileUploaderProps {
   onFileSelected: (file: File) => void
 }
 
 
 const FileUploader: React.FC<FileUploaderProps> = ({ onFileSelected }) => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [error, setError] = useState("")
-  const [file, setFile] = useState<File | null>(null)
-
-  const [dragging, setDragging] = useState(false)
-
-  const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setDragging(true)
-  }
-
-  const handleDragLeave = () => {
-    setDragging(false)
-  }
-
-  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setDragging(false)
-
-    const uploadedFile = e.dataTransfer.files[0]
-    if (uploadedFile) {
-      handleFileChange({
-        target: { files: [uploadedFile] },
-      } as unknown as ChangeEvent<HTMLInputElement>)
-    }
-  }
-
-  // const triggerFileInput = () => {
-  //     const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-  //     fileInput.click();
-  //   };
-  // my changes end
-
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0]
-    if (file) {
-      const isValid = [".xls", ".xlsx", ".csv"].some((ext) =>
-        file.name.toLowerCase().endsWith(ext),
-      )
-
-      if (!isValid) {
-        if (inputRef.current) {
-          inputRef.current.value = ""
-        }
-        alert("Please upload only Excel (.xls, .xlsx) or CSV files")
-        setError("Please upload only Excel (.xls, .xlsx) or CSV files")
-        return
-      }
-
-      setFile(file)
-      setError("")
-      onFileSelected(file)
-    }
-  }
-
+  const {handleDragLeave, handleDragOver, handleDrop, handleFileChange, error,file, inputRef, dragging} = useFileUploader({onFileSelected});
   return (
     <div className="flex flex-col items-center my-8">
     <div
