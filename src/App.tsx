b330ec32fd3ed5070094262774f6b83ce973e259
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-12-21 15:31:39 +0530

    wrap the Home Component with FileContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from './page/SignIn';
 import SignUp from './page/SignUp';
 import Hero from './page/Hero';
 import { AuthProvider } from './context/AuthContext';
+import FileContextProvider from './context/FileContext';
 function App() {
   return (
     <AuthProvider>
@@ -13,7 +14,9 @@ function App() {
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
         <Route element={<PrivateRoutes />}>
-          <Route path='/home' element={<Home />} />
+        
+          <Route path='/home' element={<FileContextProvider><Home /></FileContextProvider>} />
+        
         </Route>
       </Routes>
     </AuthProvider>
